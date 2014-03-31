8a3c3eba1ba0d721e452d2e420f6fb79fffd2fb4
gogu <gogu9909@gmail.com>
2014-03-31 10:36:06 +0800

    initial commit

---
+++ HTML/style/js/tab-style.js
@@ -0,0 +1,41 @@
+﻿/* tab标签 - index.html */
+
+//hot_tab
+
+$(function(){
+	var $beginner=$(".here_focus");
+	
+	$(".tab_box li").click(function(){
+		$beginner.removeClass("here_focus");
+		$beginner.parent().next().hide();
+		
+		$(this).children(".tab_list").show();
+		$(this).children("h3").children("a").addClass("here_focus");
+		
+		var $index=$(this).index();
+		if ($index==0){
+			$(".first_h3").css("borderLeftColor","#cdcdcd");
+		}else{
+			$(".first_h3").css("borderLeftColor","#f9f7f8");
+		}
+		
+		$beginner=$(".here_focus");
+	});
+});
+//-------------------------------------//
+
+//rank_list
+
+$(function(){
+	$(".rank_main h3").mouseover(function(){
+		$pos=$(this).position();
+		$(this).parents("ul").children(".tab_h3_bg").stop().animate({left:$pos.left},150,'swing');
+	});
+	
+	$(".rank_main li").mouseover(function(){
+		$(this).prevAll().children(".rank_list").hide();
+		$(this).nextAll().children(".rank_list").hide();
+		$(this).children(".rank_list").show();
+	});
+});
+//--------------------------------------//
---
+++ HTML/style/js/title_box.js
@@ -0,0 +1,31 @@
+﻿//titleBOX
+
+$(function(){
+	var $title,$text,$left,$right;
+	
+	$(".title_hover a").not(".def_hover").mouseover(function(){
+		$(".tips").stop();
+		$(".tips p").remove();
+		
+		$text=$(this).text();
+		$title=$(this).attr("title");
+		$(this).attr("title","");
+		
+		if ($text!="") $("<p>"+$text+"</p>").appendTo(".tip_name");
+		if ($title!="") $("<p>"+$title+"</p>").appendTo(".tip_title");
+		
+		$(this).mousemove(function(e){
+				$left=e.pageX+20;
+				$right=e.pageY+10;
+				$(".tips")
+					.show()
+					.css("left",$left).css("top",$right).css("opacity","0.8");
+		});
+	}).mouseout(function(){
+		$(".tips").stop().animate({opacity:0},240,function(){
+			$(".tips").hide();
+		});
+		
+		$(this).attr("title",$title);
+	});
+});
